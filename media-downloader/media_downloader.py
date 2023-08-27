8eda707dd427202d65c5677a6bafa76976a834c9
Saleem Toure <saleemtoure@gmail.com>
2023-08-27 14:43:20 +0200

    Working on a progress bar

--- media-downloader/media_downloader.py
+++ media-downloader/media_downloader.py
@@ -1,5 +1,7 @@
 from fileinput import filename
+from operator import le
 import os
+from re import L
 import ffmpeg
 from pytube import YouTube
 import pytube.exceptions as pte
@@ -9,82 +11,6 @@ from tkinter import messagebox
 
 desktop_path = os.path.normpath(os.path.expanduser("~/Desktop"))
 
-#! YT PART START
-
-
-# def get_media(video_url, media_type, video_quality):
-#     try:
-#         yt = YouTube(video_url)
-#         video_title = yt.title
-
-#         if media_type == "Audio":
-#             print(yt.streams.filter(only_audio=True))
-#             yt.streams.get_audio_only().download()
-#         elif video_quality == "720p":
-#             yt.streams.filter(
-#                 res="720p", mime_type="video/mp4", progressive=True
-#             ).first().download()
-#         else:
-#             yt.streams.get_audio_only().download(filename="audio.mp3")
-#             audio = ffmpeg.input("audio.mp3")
-#             if video_quality == "Highest":
-#                 yt.streams.filter(adaptive=True).order_by(
-#                     "resolution"
-#                 ).desc().first().download(filename="video.mp4")
-
-#                 video = ffmpeg.input("video.mp4")
-#                 ffmpeg.concat(video, audio, v=1, a=1).output(
-#                     f"{video_title}({yt.streams.filter(adaptive=True).order_by('resolution').desc().first().resolution}).mp4"
-#                 ).run(overwrite_output=True)
-
-#                 if os.path.exists("video.mp4") and os.path.exists("audio.mp3"):
-#                     os.remove("video.mp4")
-#                     os.remove("audio.mp3")
-#                 else:
-#                     "Error with deleting merged files"
-
-#             elif video_quality == "1080p":
-#                 yt.streams.filter(
-#                     res="1080p", mime_type="video/mp4", adaptive=True
-#                 ).first().download(filename="video.mp4")
-
-#                 video = ffmpeg.input("video.mp4")
-#                 ffmpeg.concat(video, audio, v=1, a=1).output(
-#                     f"{video_title}(1080p).mp4"
-#                 ).run(overwrite_output=True)
-
-#                 if os.path.exists("video.mp4") and os.path.exists("audio.mp3"):
-#                     os.remove("video.mp4")
-#                     os.remove("audio.mp3")
-#                 else:
-#                     "Error with deleting merged files"
-
-#             elif video_quality == "720p":
-#                 yt.streams.filter(
-#                     res="720p", mime_type="video/mp4", progressive=True
-#                 ).first().download()
-
-#     except pte.RegexMatchError:
-#         print("Regex match error. Invalid URL.")
-#     except pte.VideoPrivate:
-#         print("Video is private.")
-#     except pte.VideoRegionBlocked:
-#         print("Video is blocked in your region.")
-#     except pte.VideoUnavailable:
-#         print("Video is unavailable.")
-#     except pte.PytubeError as e:
-#         print(f"A Pytube error occurred: {e}")
-#     except Exception as e:
-#         print(f"An unexpected error occurred: {e}")
-
-
-# get_media(
-#     "https://www.youtube.com/watch?v=w2IhccXakkE&ab_channel=PoloGVEVO",
-#     "Video",
-#     "1080p",
-# )
-#! YT PART END
-
 
 class DownloaderGUI:
     def __init__(self):
@@ -93,7 +19,7 @@ class DownloaderGUI:
         self.label = tk.Label(self.root, text="YouTube Downloader", font=("Arial", 19))
         self.label.pack(padx=10, pady=10)
 
-        self.url_box = tk.Entry(self.root, width=50, font=("Arial", 16))
+        self.url_box = tk.Entry(self.root, width=60, font=("Arial", 16))
         self.url_box.bind("<KeyPress>", self.shortcut)
         self.url_box.insert(0, "Paste your URL here")
         self.url_box.pack(padx=10, pady=10)
@@ -125,6 +51,17 @@ class DownloaderGUI:
             ),
         )
         self.download_button.pack(padx=10, pady=10)
+
+        self.progress = tk.IntVar()
+        self.progress_bar = ttk.Progressbar(
+            self.root,
+            mode="determinate",
+            maximum=110,
+            length=150,
+            variable=self.progress,
+        )
+        self.progress_bar.pack(padx=10, pady=10)
+        self.progress.set(50)
 
         self.root.protocol("WM_DELETE_WINDOW", self.onClosing)
         self.root.mainloop()
