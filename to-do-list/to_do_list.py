e0983f31cacc01fac0ba2c604391fa3853440685
Saleem Toure <saleemtoure@gmail.com>
2023-09-09 00:00:29 +0200

    1. Udated to TTK 2. Added optionality to save and load todolist file

--- to-do-list/to_do_list.py
+++ to-do-list/to_do_list.py
@@ -1,51 +1,80 @@
+import os
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk, filedialog
+import sv_ttk
 
 
 class ListGUI:
     def __init__(self):
         self.root = tk.Tk()
+        self.root.title("To Do List - github.com/saleemtoure")
+        sv_ttk.set_theme("dark")
 
-        self.label = tk.Label(self.root, text="Din Liste", font=("Arial", 19))
+        self.root.maxsize(370, 500)
+        self.root.minsize(370, 500)
+
+        self.label = ttk.Label(self.root, text="TO DO", font=("Arial", 19))
         self.label.pack(padx=10, pady=10)
 
-        self.entrybox = tk.Entry(self.root, font=("Arial", 16), width=20)
+        self.entrybox = ttk.Entry(self.root, font=("Arial", 16), width=20)
         self.entrybox.bind("<Return>", self.add_objective)
         self.entrybox.pack(padx=10, pady=10)
 
-        self.scrollbar = tk.Scrollbar(self.root)
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
-        self.listbox = tk.Listbox(
-            self.root, font=("Arial", 16), activestyle="dotbox", width=20
-        )
-        self.listbox.pack(padx=10, pady=10)
-        self.listbox.config(yscrollcommand=self.scrollbar.set)
-        self.listbox.bind("<<ListboxSelect>>", self.delete_objective)
-        self.scrollbar.config(command=self.listbox.yview)
+        self.listbox = ttk.Treeview(self.root, show="tree")
+        self.listbox.pack(padx=10, pady=10, fill="both", expand=True)
 
-        self.clear_btn = tk.Button(text="Clear", padx=10, pady=10, command=self.reset)
+        self.listbox.bind("<<TreeviewSelect>>", self.delete_objective)
+
+        self.out_btn = ttk.Button(text="Save as txt file", command=self.save)
+        self.out_btn.pack(pady=2)
+
+        self.inn_btn = ttk.Button(text="Load list from txt file", command=self.load)
+        self.inn_btn.pack()
+
+        self.clear_btn = ttk.Button(text="Clear", command=self.reset)
         self.clear_btn.pack(padx=10, pady=10)
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.mainloop()
 
     def add_objective(self, event):
-        self.listbox.insert("end", self.entrybox.get())
+        self.listbox.insert("", "end", text=self.entrybox.get())
 
     def delete_objective(self, event):
-        if self.listbox.curselection() == ():
+        if self.listbox.selection() == ():
             pass
         else:
             if messagebox.askyesno(
-                title="Quit?", message="Vil du slette denne tasken?"
+                title="Quit?", message="Do you want to delete this task?"
             ):
-                self.listbox.delete(self.listbox.curselection())
+                self.listbox.delete(self.listbox.selection())
+
+    def save(self):
+        file_path = filedialog.asksaveasfilename(
+            defaultextension=".txt", filetypes=[("Text Files", "*.txt")]
+        )
+        if file_path == "":
+            pass
+        elif os.path.exists(file_path):
+            f = open(file_path, "w")
+            for task in self.listbox.get_children():
+                f.write(f"{self.listbox.item(task)['text']}\n")
+            f.close()
+
+    def load(self):
+        f = open(f"{filedialog.askopenfilename()}", "r")
+        self.reset()
+        for line in f:
+            self.listbox.insert("", "end", text=line)
+        f.close()
 
     def reset(self):
-        self.listbox.delete(0, "end")
+        for task in self.listbox.get_children():
+            self.listbox.delete(task)
 
     def on_closing(self):
-        if messagebox.askyesno(title="Quit?", message="Vil du virkelig avslutte"):
+        if messagebox.askyesno(title="Quit?", message="Do you really want to quit?"):
             self.root.destroy()
 
 
